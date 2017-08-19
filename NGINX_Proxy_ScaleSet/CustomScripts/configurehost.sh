30ae501dfeab94dde2eeacd609c05d834c158ccc
Mark Hedley <mahedley@microsoft.com>
2017-08-19 14:51:20 -0700

    Added my own init.d script

--- NGINX_Proxy_ScaleSet/CustomScripts/configurehost.sh
+++ NGINX_Proxy_ScaleSet/CustomScripts/configurehost.sh
@@ -1,12 +1,12 @@
 # Custom Script for Linux
 apt-get update
 apt-get install -y nginx
-apt-get install -y gettext
+apt-get install -y curl
 curl https://raw.githubusercontent.com/mahedley/NGINX-Proxy-Scaleset/master/NGINX_Proxy_ScaleSet/CustomScripts/nginx.config > /etc/nginx/conf.d/nginx_config.conf
-curl https://raw.githubusercontent.com/JasonGiedymin/nginx-init-ubuntu/master/nginx > /etc/init.d/nginx
+curl https://raw.githubusercontent.com/mahedley/NGINX-Proxy-Scaleset/master/NGINX_Proxy_ScaleSet/CustomScripts/nginx_init> /etc/init.d/nginx
 chmod +x /etc/init.d/nginx
-update-rc.d /usr/sbin/update-rc.d -f defaults
-sudo /etc/init.d/nginx start
+update-rc.d -f nginx defaults
+/etc/init.d/nginx start
 
 
 
