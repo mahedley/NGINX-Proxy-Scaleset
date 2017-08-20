afcf588c52d9b72b0bf60547411e4e5ee31bd78e
Mark Hedley <mahedley@microsoft.com>
2017-08-19 19:49:50 -0700

    Debugged init,config, and deploy

--- NGINX_Proxy_ScaleSet/CustomScripts/configurehost.sh
+++ NGINX_Proxy_ScaleSet/CustomScripts/configurehost.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install -y nginx
 apt-get install -y curl
-curl https://raw.githubusercontent.com/mahedley/NGINX-Proxy-Scaleset/master/NGINX_Proxy_ScaleSet/CustomScripts/nginx.config > /etc/nginx/conf.d/nginx_config.conf
+curl https://raw.githubusercontent.com/mahedley/NGINX-Proxy-Scaleset/master/NGINX_Proxy_ScaleSet/CustomScripts/nginx.config > /etc/nginx/nginx.conf
 curl https://raw.githubusercontent.com/mahedley/NGINX-Proxy-Scaleset/master/NGINX_Proxy_ScaleSet/CustomScripts/nginx_init> /etc/init.d/nginx
 chmod +x /etc/init.d/nginx
 update-rc.d -f nginx defaults
--- NGINX_Proxy_ScaleSet/Deploy-AzureResourceGroup.ps1
+++ NGINX_Proxy_ScaleSet/Deploy-AzureResourceGroup.ps1
@@ -8,8 +8,8 @@ Param(
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
-    [string] $TemplateFile = 'LinuxVirtualMachineScaleSet.json',
-    [string] $TemplateParametersFile = 'LinuxVirtualMachineScaleSet.parameters.json',
+    [string] $TemplateFile = 'azuredeploy.json',
+    [string] $TemplateParametersFile = 'azuredeploy.parameters.json',
     [string] $ArtifactStagingDirectory = '.',
     [string] $DSCSourceFolder = 'DSC',
     [switch] $ValidateOnly
